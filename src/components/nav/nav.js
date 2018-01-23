f5c4d1e6268997ad302d0047d9a8250cdaa1cc08
Victoria Yorgesen <victoria.yorgesen1@gmail.com>
2018-01-23 15:43:05 -0800

    added cart pic

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import './nav.css';
 import Hamburger from '../../imgs/hamburger';
-
+import Cart from '../../imgs/cart.js';
 export default function Nav (){
     return(
         <div className="navDiv">
             <div className="navDivRight">
                  <div className="etsyLogo">Etsy</div>
-                 <div>Hambur</div>
+                 <div className="hamburger"><Hamburger/></div>
             </div>
             <div className="navDivLeft">
                 <div>Sell</div>
                 <div>Sign in</div>
-                <div>Cart</div>
+                <div className="cart"><Cart/></div>
             </div>
         </div>
     )
